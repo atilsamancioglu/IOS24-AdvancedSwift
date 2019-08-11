87f9306ad1d85c12d7f30e93b974ee10ebe91037
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-11 16:59:42 +0300

    Initial Commit

---
+++ AdvancedSwiftProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedSwiftProject
+//
+//  Created by Atil Samancioglu on 11.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
