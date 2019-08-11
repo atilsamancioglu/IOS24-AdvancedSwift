55d67ddaf61cde4596fe1c128cf1da1182d0fe58
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-11 17:45:14 +0300

    first commit

---
+++ AdvancedSwiftProject/MusicianClass.swift
@@ -0,0 +1,9 @@
+//
+//  MusicianClass.swift
+//  AdvancedSwiftProject
+//
+//  Created by Atil Samancioglu on 11.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# IOS24-AdvancedSwift
